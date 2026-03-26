04f9a75029c9b0c03a6b81620795757d4419c5ed
KurkchiSergey <153356949+KurkchiSergey@users.noreply.github.com>
2026-03-26 11:53:58 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        /*Задание 1. Напишите программу, которая при запуске выводит ваше имя и фамилию на латинице. */
+        System.out.println("Sergei Kurkchi");
+
+    }
+}
